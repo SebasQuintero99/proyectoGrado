a2e19d1455ea6d2fad26c1fcfd0a3694104ea36d
Shuraldy Garrido <schuraldy120@gmail.com>
2025-01-20 13:17:15 -0500

    filtro

--- controllers/handleErrorLogin.js
+++ controllers/handleErrorLogin.js
@@ -5,7 +5,7 @@ exports.getErrorMessage = (errorType) => {
         userNotFound: 'No se encontró un usuario con el correo proporcionado.',
         serverError: 'Ocurrió un error en el servidor. Intenta nuevamente más tarde.',
         tokenExpired: 'Tu sesión ha expirado. Por favor, inicia sesión nuevamente.',
-        unauthorized: 'No tienes autorización para acceder a esta ruta.',
+        unauthorized: 'No tienes autorización para acceder a esta ruta .',
     };
 
     return errorMessages[errorType] || 'Ocurrió un error desconocido.';
